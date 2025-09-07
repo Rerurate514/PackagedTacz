ea616401412f61aac855311d8c38dd5799483792
Rerurate514 <rerurateyuto@gmail.com>
2025-09-07 11:12:47 +0900

    テクスチャを変更

--- src/main/kotlin/com/rerurate/packagedtacz/blockEntities/TaczMolecularAssemblerBlockEntity.kt
+++ src/main/kotlin/com/rerurate/packagedtacz/blockEntities/TaczMolecularAssemblerBlockEntity.kt
@@ -5,20 +5,28 @@ import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.core.Direction
+import net.minecraft.world.item.ItemStack
+import net.minecraftforge.common.capabilities.Capability
+import net.minecraftforge.common.capabilities.ForgeCapabilities
+import net.minecraftforge.common.util.LazyOptional
+import net.minecraftforge.items.IItemHandler
+import net.minecraftforge.items.ItemStackHandler
 
 class TaczMolecularAssemblerBlockEntity(pos: BlockPos, state: BlockState) :
     BlockEntity(PackagedTaczBlockEntities.TACZ_MOLECULAR_ASSEMBLER.get(), pos, state) {
-
     var yourData: Int = 0
+    private val itemHandler = ItemStackHandler(4)
+    private val itemHandlerOptional = LazyOptional.of { itemHandler }
 
     override fun saveAdditional(tag: CompoundTag) {
-        tag.putInt("tacz_molecular_assembler_data", yourData)
+        tag.put("inventory", itemHandler.serializeNBT())
         super.saveAdditional(tag)
     }
 
     override fun load(tag: CompoundTag) {
         super.load(tag)
-        yourData = tag.getInt("tacz_molecular_assembler_data")
+        itemHandler.deserializeNBT(tag.getCompound("inventory"))
     }
 
     override fun getUpdatePacket(): ClientboundBlockEntityDataPacket {
@@ -27,5 +35,17 @@ class TaczMolecularAssemblerBlockEntity(pos: BlockPos, state: BlockState) :
 
     override fun getUpdateTag(): CompoundTag {
         return this.saveWithoutMetadata()
+    }
+
+    override fun <T> getCapability(cap: Capability<T>, side: Direction?): LazyOptional<T> {
+        if (cap == ForgeCapabilities.ITEM_HANDLER) {
+            return itemHandlerOptional.cast()
+        }
+        return super.getCapability(cap, side)
+    }
+
+    override fun invalidateCaps() {
+        super.invalidateCaps()
+        itemHandlerOptional.invalidate()
     }
 }
