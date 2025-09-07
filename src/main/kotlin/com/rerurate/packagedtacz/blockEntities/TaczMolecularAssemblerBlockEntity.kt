36d0dc182496c4ce657e31051d32992f34b24391
Rerurate514 <rerurateyuto@gmail.com>
2025-09-07 11:15:51 +0900

    アイテムスタック数を変更

--- src/main/kotlin/com/rerurate/packagedtacz/blockEntities/TaczMolecularAssemblerBlockEntity.kt
+++ src/main/kotlin/com/rerurate/packagedtacz/blockEntities/TaczMolecularAssemblerBlockEntity.kt
@@ -6,17 +6,14 @@ import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.core.Direction
-import net.minecraft.world.item.ItemStack
 import net.minecraftforge.common.capabilities.Capability
 import net.minecraftforge.common.capabilities.ForgeCapabilities
 import net.minecraftforge.common.util.LazyOptional
-import net.minecraftforge.items.IItemHandler
 import net.minecraftforge.items.ItemStackHandler
 
 class TaczMolecularAssemblerBlockEntity(pos: BlockPos, state: BlockState) :
     BlockEntity(PackagedTaczBlockEntities.TACZ_MOLECULAR_ASSEMBLER.get(), pos, state) {
-    var yourData: Int = 0
-    private val itemHandler = ItemStackHandler(4)
+    private val itemHandler = ItemStackHandler(256)
     private val itemHandlerOptional = LazyOptional.of { itemHandler }
 
     override fun saveAdditional(tag: CompoundTag) {
